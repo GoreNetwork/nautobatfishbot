7274003b6eb277d7b1de00d9664c40fad1f98d5d
Base Ubuntu <dhimes@gmail.com>
2021-04-28 18:06:55 -0700

    TSing

---
+++ nautobatfishbot/navigation.py
@@ -0,0 +1,12 @@
+# # navigation.py
+# from nautobot.extras.plugins import PluginMenuButton, PluginMenuItem
+# from nautobot.utilities.choices import ButtonColorChoices
+
+
+# menu_items = (
+#     PluginMenuItem(
+#         link='plugins:nautobatfishbot',
+#         link_text='nautobatfishbot',
+       
+#     ),
+# )
--- nautobatfishbot/urls.py
+++ nautobatfishbot/urls.py
@@ -1,7 +1,10 @@
 """Django urlpatterns declaration for nautobatfishbot plugin."""
 from django.urls import path, include
 from django.contrib import admin
-# from . import views
+from . import views
 
 urlpatterns = [
     path('nautobatfishbot/', views.nautobatfishbot, name="nautobatfishbot_homepage")]
+
+# urlpatterns = [
+#     path('nautobatfishbot/', views.nautobatfishbot.as_view(), name="nautobatfishbot_homepage")]
